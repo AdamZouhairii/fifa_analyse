496f38dee85ae3c2c2d1f03a6f01d8dbde762dbf
vvadamvv <97172783+vvadamvv@users.noreply.github.com>
2022-12-11 17:17:00 +0100

    Update fut.py

--- pack fifa/fut.py
+++ pack fifa/fut.py
@@ -4,20 +4,28 @@ import pandas as pd
 import requests
 
 from bs4 import BeautifulSoup
+ 
 
-# je donne l'url et les colonne
+
 base_url = "https://sofifa.com/players?offset="
-source_code = requests.get(base_url)
-texte = source_code.text
-soup = BeautifulSoup(texte, 'html.parser')
-table_body = soup.find('tbody')
-for row in table_body.findAll('tr'):
-    td = row.findAll('td')
-    name = td[1].find("a").get("href")
-    url='https://sofifa.com'+name
-    html=requests.get(url)
-    tex=html.text
-    soupe=BeautifulSoup(tex, 'html.parser')
-    table=soupe.find("li",class_='ellipsis')
-    stat=soupe.find('h5')
-    print(stat.text)
+ 
+
+for i in range(0, 1):
+    url = base_url + str(i * 60)
+    source_code = requests.get(url)
+    texte = source_code.text
+    soup = BeautifulSoup(texte, 'html.parser')
+    table_body = soup.find('tbody')
+    for row in table_body.findAll('tr'):
+        td = row.findAll('td')
+        name = td[1].find("a").get("href")
+        url='https://sofifa.com'+name
+        html=requests.get(url)
+        tex=html.text
+        soupe=BeautifulSoup(tex, 'html.parser')
+        for row in soupe.findAll('div',class_='block-quarter'):
+            for row in row.findAll('ul',class_='pl' ):
+                for row in row.findAll('li'):
+                    gendetail=row.find('span')
+                    print(gendetail)
+           
